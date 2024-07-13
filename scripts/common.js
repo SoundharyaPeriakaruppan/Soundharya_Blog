9d3123f2d5a4f992b28581599a7b79fab87f8dc6
MuruVr <soundperi@gmail.com>
2024-07-13 20:02:09 +0530

    Additional fixes.

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,6 @@
 
 window.addEventListener("load", function() {
-    // Code from script1.js 
+    //Header generation
     let headerHtml = document.getElementById("header");
      let header=`<div class="header"><div class='logo' style="display: flex;align-items: center;">
      <a href='/index.html'>
@@ -9,4 +9,33 @@ window.addEventListener("load", function() {
      </div>`;
      header+="<nav  class='nav' style='display: flex;justify-content: center;align-items: center;'>"+"<a href=/index.html>"+"Home"+"</a>"+"<a href=./project/project.html>"+"Projects"+"</a>"+"<a href=/blog.html>"+"Blog"+"</a>"+"</nav></div>";
      headerHtml.innerHTML+=header;
+
+    //Generating Table Of Contents
+    var toc = document.getElementById('table-of-contents');
+    if (toc){
+        var title = document.createElement('h3');
+        title.className = 'title text-red toc-title';
+        title.textContent = 'Table of Contents';
+        toc.appendChild(title);
+
+        var headings = Array.from(document.querySelectorAll('h3:not(.toc-title), h4'));
+        headings.forEach(function(heading, index) {
+            var link = document.createElement('a');
+            var id = heading.textContent.toLowerCase().replace(/\s+/g, '-');
+            link.href = '#' + id;
+            link.textContent = heading.textContent;
+            heading.id = id;
+
+            var listItem = document.createElement('li');
+            listItem.appendChild(link);
+
+            if (heading.tagName.toLowerCase() === 'h4') {
+                listItem.className = 'sub-heading';
+                listItem.style.marginLeft = '20px';
+            }else{
+                listItem.className = 'heading';
+            }
+            toc.appendChild(listItem);
+        });
+    }
  });
